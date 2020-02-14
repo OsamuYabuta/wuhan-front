a0bb746df8a0f3a9f88602cc857760ac9630c188
OsamuYabuta <cheshacatmark2@gmail.com>
2020-02-14 21:39:14 +0900

    added build dir

---
+++ build/precache-manifest.7da99f978adf281816d546fadf31928a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "552dae0113eb85c09684599c0808d6e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d59867a45ff9ddb2fcc",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "c96e360817d0a0e8449d",
+    "url": "/static/css/main.d878f225.chunk.css"
+  },
+  {
+    "revision": "6d59867a45ff9ddb2fcc",
+    "url": "/static/js/2.13f587b1.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/static/js/2.13f587b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c96e360817d0a0e8449d",
+    "url": "/static/js/main.7e61d77f.chunk.js"
+  },
+  {
+    "revision": "cdf041b3892fd22a989b",
+    "url": "/static/js/runtime-main.6c427cc5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
